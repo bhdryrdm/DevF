960f75488409aecd463594820ab4d0a6a9477ccf
bhdryrdm <bhdryrdm@gmail.com>
2018-04-22 23:34:09 +0300

    Broken Auth tablar tamamlandı

---
+++ DevF_LAB/DevF_LABS.Presentation/Controllers/BrokenAuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevF_LABS.Presentation.Controllers
+{
+    public class BrokenAuthController : Controller
+    {
+        // GET: BrokenAuth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

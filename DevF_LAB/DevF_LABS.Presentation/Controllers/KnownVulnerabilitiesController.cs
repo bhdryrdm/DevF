5728f196f3545da0cf62306eb1687056ebfed75a
Recep Yapıcı <recepypc@hotmail.com>
2018-04-22 23:20:51 +0300

    Controller'lar eklenip barlar düzenlendi.

---
+++ DevF_LAB/DevF_LABS.Presentation/Controllers/KnownVulnerabilitiesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevF_LABS.Presentation.Controllers
+{
+    public class KnownVulnerabilitiesController : Controller
+    {
+        // GET: KnownVulnerabilities
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
